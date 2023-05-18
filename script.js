7cbcaf419cd6b6cb10b35655f79eef82546c1f55
Agustin Minzoni <agustinminzoni@gmail.com>
2023-05-18 15:56:28 -0300

    avance 18-05

---
+++ script.js
@@ -0,0 +1,25 @@
+const API_URL = "https://api.adviceslip.com/advice";
+const button = document.getElementById('random');
+
+const traerFrase = () => {
+    fetch(`${API_URL}`)
+        .then((response) => response.json())
+        .then((device) => {
+            //Traigo la informacion a mostrar
+            const adviceID = device.slip.id;
+            const advice = device.slip.advice;
+
+            //Le digo donde mostrar la informacion
+            document.getElementById('adviceID').textContent = adviceID;
+            document.getElementById('advice').textContent = advice;
+        })
+        .catch(error => {
+            console.log('Error', error);
+        });
+};
+
+button.addEventListener("click", function () {
+    traerFrase();
+});
+
+traerFrase();
